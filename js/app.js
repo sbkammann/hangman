802e0bec368195f9f3cab63b39c76cce08627b35
Shannan Kammann <sbkammann@gmail.com>
2018-12-13 16:40:08 -0800

    feat: Added check function
    check function checks if the player's guess is right

--- js/app.js
+++ js/app.js
@@ -15,13 +15,30 @@ for (let i = 0; i < charArr.length; i++){
 // creates alphabet buttons for user to press
 for (let i = 65; i < 91; i++){
   const key = document.createElement('div');
-  key.setAttribute('class', `key${i}`+' '+'key');
+  key.setAttribute('class', `${String.fromCharCode(i)}`+' '+'key');
   key.textContent = String.fromCharCode(i);
   if (i < 78) {
     rowOne.appendChild(key);
   }
   else {
     rowTwo.appendChild(key);
+  }
+}
+
+buttonBox.addEventListener('click', check);
+
+//checks if the selected character button matches a letter in the word
+function check() {
+  let isMatch = false;
+  for (let i= 0; i < charArr.length; i++){
+    if(event.target.getAttribute('class')[0].toUpperCase() === charArr[i].toUpperCase()){
+      console.log(document.getElementsByClassName(`char${i}`)[0]);
+      document.getElementsByClassName(`char${i}`)[0].textContent = charArr[i].toUpperCase();
+      isMatch = true;
+    }
+    if ((i === charArr.length -1) && !isMatch){
+      console.log('wrong');
+    }
   }
 }
 //window.innerWidth
